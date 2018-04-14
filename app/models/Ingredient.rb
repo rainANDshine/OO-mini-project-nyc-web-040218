aba66182894518dfa2ce066a13477f1402098928
Shun Yao <ysmiracle@hotmail.com>
2018-04-14 15:19:31 +0000

    automatically backed up by learn

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,21 @@
+class Ingredient
 
+attr_reader :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def all
+  @@all
+end
+
+def self.most_common_allergen
+  names = self.all.map {|e| e.name}
+  names.max_by { |i| names.count(i) }
+end
+
+end
